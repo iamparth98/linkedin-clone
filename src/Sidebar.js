d2e3e935a89a6ceda4d27796a5ad7c32f6beb6fe
parth tiwari <parthtiwari98@gmail.com>
2022-08-23 01:43:52 +0530

    working on sidebar

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -1,7 +1,31 @@
+import { Avatar } from "@mui/material";
 import React from "react";
+import "./Sidebar.css";
 
 function Sidebar() {
-  return <div className="sidebar">Sidebar</div>;
+  return (
+    <section className="sidebar">
+      <section className="sidebar__top">
+        <img src="" alt="" />
+        <Avatar className="sidebar__top__avatar" />
+        <h2>Parth Tiwari</h2>
+        <h4>parthtiwari98@gmail.com</h4>
+        <section className="sidebar__stats">
+          <section className="sidebar__stat">
+            <p>Who viewed you</p>
+            <p className="sidebar__stat__number">2500</p>
+          </section>
+          <section className="sidebar__stat">
+            <p>Who viewed you</p>
+            <p className="sidebar__stat__number">2500</p>
+          </section>
+        </section>
+      </section>
+      <section className="sidebar__bottom">
+        <p>Recents</p>
+      </section>
+    </section>
+  );
 }
 
 export default Sidebar;
