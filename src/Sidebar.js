f336b99e641ac032f3d353ebff876e1a1ccfb52d
parth tiwari <parthtiwari98@gmail.com>
2022-08-23 18:04:13 +0530

    Building sidebar bottom

--- src/Sidebar.js
+++ src/Sidebar.js
@@ -6,21 +6,26 @@ function Sidebar() {
   return (
     <section className="sidebar">
       <section className="sidebar__top">
-        <img src="" alt="" />
+        <img
+          src="https://images.freeimages.com/images/large-previews/01a/technology-background-1632715.jpg"
+          alt=""
+        />
         <Avatar className="sidebar__top__avatar" />
         <h2>Parth Tiwari</h2>
         <h4>parthtiwari98@gmail.com</h4>
-        <section className="sidebar__stats">
-          <section className="sidebar__stat">
-            <p>Who viewed you</p>
-            <p className="sidebar__stat__number">2500</p>
-          </section>
-          <section className="sidebar__stat">
-            <p>Who viewed you</p>
-            <p className="sidebar__stat__number">2500</p>
-          </section>
+      </section>
+
+      <section className="sidebar__stats">
+        <section className="sidebar__stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__stat__number">2500</p>
+        </section>
+        <section className="sidebar__stat">
+          <p>Views on posts</p>
+          <p className="sidebar__stat__number">2500</p>
         </section>
       </section>
+
       <section className="sidebar__bottom">
         <p>Recents</p>
       </section>
