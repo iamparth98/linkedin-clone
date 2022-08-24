e5def23c78b14913b0af56a13d985d294ee2029c
parth tiwari <parthtiwari98@gmail.com>
2022-08-24 16:31:48 +0530

    feed-Input-section-complete

---
+++ src/Feed.js
@@ -0,0 +1,37 @@
+import React from "react";
+import "./Feed.css";
+import InputOption from "./InputOption";
+import CreateIcon from "@mui/icons-material/Create";
+import ImageIcon from "@mui/icons-material/Image";
+import SubscriptionsIcon from "@mui/icons-material/Subscriptions";
+import EventNoteIcon from "@mui/icons-material/EventNote";
+import CalendarViewDayIcon from "@mui/icons-material/CalendarViewDay";
+
+function Feed() {
+  return (
+    <section className="feed">
+      <section className="feed__input__container">
+        <section className="feed__input">
+          <CreateIcon />
+          <form>
+            <input type="text" name="" id="" />
+            <button type="submit">submit</button>
+          </form>
+        </section>
+
+        <section className="feed__input__option">
+          <InputOption Icon={ImageIcon} title="Photo" color="#70B5F9" />
+          <InputOption Icon={SubscriptionsIcon} title="Video" color="#E7a33E" />
+          <InputOption Icon={EventNoteIcon} title="Event" color="#C0CBCD" />
+          <InputOption
+            Icon={CalendarViewDayIcon}
+            title="Write Article"
+            color="#7FC15E"
+          />
+        </section>
+      </section>
+    </section>
+  );
+}
+
+export default Feed;
